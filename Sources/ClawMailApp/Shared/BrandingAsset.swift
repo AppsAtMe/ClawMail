70ebecd01442f320d2ac7984f040af7e09e1c803
Max Headroom <maxheadroom@maxs.mac.mini.lan>
2026-03-09 15:18:09 -0700

    fix: unique raw values for BrandingAsset enum

--- Sources/ClawMailApp/Shared/BrandingAsset.swift
+++ Sources/ClawMailApp/Shared/BrandingAsset.swift
@@ -4,10 +4,20 @@ import Foundation
 enum BrandingAsset: String, CaseIterable {
     case appIconArtwork = "AppIcon-New"
     case splashArtwork = "AboutArtwork"
-    case splashArtworkSquare = "AboutArtwork"
+    case splashArtworkSquare = "AboutArtwork-Square"
+
+    var filename: String {
+        // Both splash variants use the same AboutArtwork image
+        switch self {
+        case .splashArtworkSquare:
+            return "AboutArtwork"
+        default:
+            return rawValue
+        }
+    }
 
     var url: URL? {
-        Bundle.main.url(forResource: rawValue, withExtension: "png", subdirectory: "Branding")
+        Bundle.main.url(forResource: filename, withExtension: "png", subdirectory: "Branding")
     }
 
     var image: NSImage? {
